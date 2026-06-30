scottgl9/sglang

gb10: add sglang.sh launch script and GB10 SM121 fixes

python/sglang/srt/server_args.py
@@ -1809,20 +1809,39 @@ class ServerArgs:
                     sm100_default_attention_backend=sm100_default_attn_backend,
                 )
 
-                # SM12.x: auto-select flashinfer for GatedDeltaNet linear attention.
-                # FlashInfer GDN CUTLASS kernels require SM90+ (capability[0] >= 9)
-                # which includes SM120. The Triton fallback lacks pipelining for
-                # the recurrent SSM state update, making flashinfer faster on Blackwell.
+                # SM12.x: auto-select flashinfer for GatedDeltaNet linear attention
+                # ONLY if the device has enough shared memory (>=228 KB optin).
+                # FlashInfer GDN CUTLASS kernels are compiled for SM90a and use
+                # large shared memory tiles.  Full Blackwell (B200/B100) has
+                # 228 KB, but GB10 has only ~99 KB, so the kernel's
+                # cudaFuncSetAttribute call fails ("initialize failed").
                 if (
                     is_sm120_supported()
                     and is_flashinfer_available()
                     and self.linear_attn_backend == "triton"
                 ):
-                    self.linear_attn_backend = "flashinfer"
-                    logger.info(
-                        "Auto-selected flashinfer linear attention backend for SM120 "
-                        f"({model_arch}): FlashInfer GDN CUTLASS kernels support SM90+."
+                    import ctypes
+
+                    _librt = ctypes.cdll.LoadLibrary("libcudart.so")
+                    _smem = ctypes.c_int()
+                    # cudaDevAttrMaxSharedMemoryPerBlockOptin = 97
+                    _librt.cudaDeviceGetAttribute(
+                        ctypes.byref(_smem), 97, 0
                     )
+                    if _smem.value >= 228 * 1024:
+                        self.linear_attn_backend = "flashinfer"
+                        logger.info(
+                            "Auto-selected flashinfer linear attention backend "
+                            f"for SM120 ({model_arch}): shared_mem_optin="
+                            f"{_smem.value} bytes."
+                        )
+                    else:
+                        logger.info(
+                            "Keeping triton linear attention backend for SM120 "
+                            f"({model_arch}): shared_mem_optin={_smem.value} "
+                            "bytes (< 228 KB required by FlashInfer GDN "
+                            "CUTLASS kernels)."
+                        )
 
         elif model_arch in ["Glm4MoeForCausalLM"]:
             if is_sm100_supported():
@@ -3266,11 +3285,19 @@ class ServerArgs:
             action="store_true",
             help="Whether to use a CausalLM as an embedding model.",
         )
-        parser.add_argument(
+        mm_group = parser.add_mutually_exclusive_group()
+        mm_group.add_argument(
             "--enable-multimodal",
+            dest="enable_multimodal",
             default=ServerArgs.enable_multimodal,
             action="store_true",
             help="Enable the multimodal functionality for the served model. If the model being served is not multimodal, nothing will happen",
+        )
+        mm_group.add_argument(
+            "--disable-multimodal",
+            dest="enable_multimodal",
+            action="store_false",
+            help="Disable multimodal: skip loading the visual encoder to save memory. Text-only serving.",
         )
         parser.add_argument(
             "--revision",

scottgl9/sglang-spark-gb10-optimizations

python/sglang/srt/server_args.py
@@ -4382,27 +4382,46 @@ class ServerArgs:
 
                 self._handle_mamba_radix_cache(model_arch=model_arch)
 
+                # SM12.x: auto-select flashinfer for GatedDeltaNet linear attention
+                # ONLY if the device has enough shared memory (>=228 KB optin).
+                # FlashInfer GDN CUTLASS kernels are compiled for SM90a and use
+                # large shared memory tiles.  Full Blackwell (B200/B100) has
+                # 228 KB, but GB10 has only ~99 KB, so the kernel's
+                # cudaFuncSetAttribute call fails ("initialize failed").
+                if (
+                    is_sm120_supported()
+                    and is_flashinfer_available()
+                    and self.linear_attn_backend == "triton"
+                ):
+                    import ctypes
+
+                    _librt = ctypes.cdll.LoadLibrary("libcudart.so")
+                    _smem = ctypes.c_int()
+                    # cudaDevAttrMaxSharedMemoryPerBlockOptin = 97
+                    _librt.cudaDeviceGetAttribute(
+                        ctypes.byref(_smem), 97, 0
+                    )
+                    if _smem.value >= 228 * 1024:
+                        self.linear_attn_backend = "flashinfer"
+                        logger.info(
+                            "Auto-selected flashinfer linear attention backend "
+                            f"for SM120 ({model_arch}): shared_mem_optin="
+                            f"{_smem.value} bytes."
+                        )
+                    else:
+                        logger.info(
+                            "Keeping triton linear attention backend for SM120 "
+                            f"({model_arch}): shared_mem_optin={_smem.value} "
+                            "bytes (< 228 KB required by FlashInfer GDN "
+                            "CUTLASS kernels)."
+                        )
+
         elif model_arch == "MiniCPMV4_6ForConditionalGeneration":
             # 4.6 wraps a Qwen3.5 hybrid GDN backbone, so it needs the same
             # mamba radix cache handling as Qwen3_5ForConditionalGeneration.
             if is_sm100_supported() and self.attention_backend is None:
                 self.attention_backend = "triton"
             self._handle_mamba_radix_cache(model_arch=model_arch)
-
-                # SM12.x: auto-select flashinfer for GatedDeltaNet linear attention.
-                # FlashInfer GDN CUTLASS kernels require SM90+ (capability[0] >= 9)
-                # which includes SM120. The Triton fallback lacks pipelining for
-                # the recurrent SSM state update, making flashinfer faster on Blackwell.
-                if (
-                    is_sm120_supported()
-                    and is_flashinfer_available()
-                    and self.linear_attn_backend == "triton"
-                ):
-                    self.linear_attn_backend = "flashinfer"
-                    logger.info(
-                        "Auto-selected flashinfer linear attention backend for SM120 "
-                        f"({model_arch}): FlashInfer GDN CUTLASS kernels support SM90+."
-                    )
 
         elif model_arch in ["Glm4MoeForCausalLM"]:
             if is_sm100_supported():
@@ -6583,6 +6602,17 @@ class ServerArgs:
 
         # Auto-derived from Annotated[..., Arg(...)] field metadata.
         add_cli_args_from_dataclass(parser, ServerArgs)
+
+        # GB10: --enable-multimodal is auto-derived above (store_true, tri-state
+        # default None = auto-detect from model config). Upstream has no way to
+        # force-disable multimodal from the CLI, so add the companion flag here
+        # to skip loading the vision tower and save memory on text-only serving.
+        parser.add_argument(
+            "--disable-multimodal",
+            dest="enable_multimodal",
+            action="store_false",
+            help="Disable multimodal: skip loading the visual encoder to save memory. Text-only serving.",
+        )
 
         # --- Fields with dynamic choices (computed at add_cli_args time) ---
         reasoning_parser_choices = list(ReasoningParser.DetectorMap.keys())